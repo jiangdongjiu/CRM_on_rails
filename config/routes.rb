b4c4d2f6bf1352311f03ad0920323d2660d49437
jiangdongjiu <jiangdongjiu5@gmail.com>
2020-11-07 14:43:17 -0600

    create views file and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: 'customer#index'
+
+  resources :customers, only: [:index] do
+    collection do
+      get 'missing_email'
+    end
+
+    collection do
+      get 'alphabetized'
+    end
+  end
+  
 end
