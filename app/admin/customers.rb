5fcd99cab56bf53ce997134d8bbdf5b58fb47389
jiangdongjiu <jiangdongjiu5@gmail.com>
2020-11-06 20:09:33 -0600

    change default form of activeadmin using formastic gem

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -2,4 +2,15 @@ ActiveAdmin.register Customer do
   # whitelisting
   permit_params :full_name, :phone, :email, :notes, :image
 
+  # DSL Domain Specific Language
+  # Formtastic gem
+  form do |f|
+    f.semantic_errors # shows errors on :base
+    f.inputs          # builds an input field for every attribute
+    f.inputs do
+      f.input :image, as: :file
+    end
+    f.actions         # adds the 'Submit' and 'Cancel' buttons
+  end
+
 end
